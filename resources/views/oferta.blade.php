b1f898959ea4f41e3807dbf8eab5f21345c69cf7
Sambrana Ivan <sambranaivan@gmail.com>
2018-10-04 17:59:32 -0300

    routas y vistas para ofertas

---
+++ resources/views/oferta.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+////lista ofertas publicadas
+
+    </div>
+</div>
+@endsection
