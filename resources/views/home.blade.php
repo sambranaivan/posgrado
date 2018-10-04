7a190818ae3a44eac66bef3a8c69672285e2a8ec
Sambrana Ivan <sambranaivan@gmail.com>
2018-10-04 16:40:15 -0300

    admin home changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Panel de Administracion</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,22 @@
                         </div>
                     @endif
 
-                    You are logged in!
+
+                    <div class="row">
+                        <div class="col col-md-4">
+                            <button type="button" class="btn btn-primary">Gestionar Ofertas</button>
+                        </div>
+
+                            <div class="col col-md-4">
+                                <button type="button" class="btn btn-primary">Gestionar Carreras</button>
+                            </div>
+
+
+                            <div class="col col-md-4">
+                                <button type="button" class="btn btn-primary">Gestionar Resoluciones</button>
+                            </div>
+
+                    </div>
                 </div>
             </div>
         </div>
