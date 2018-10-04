f6483ad836afe9c3a9276cf449ac556c98173e87
Sambrana Ivan <sambranaivan@gmail.com>
2018-10-04 16:33:58 -0300

    add Unidad model

---
+++ database/migrations/2018_10_04_186031_create_unidads_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUnidadsTable extends Migration
+{
+    /**
+     * UNIDAD ACADEMICA
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('unidads', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre');
+            $table->string('link');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('unidads');
+    }
+}
