054157a74186b0a125cdb0cdea2edf08f9e79e2e
Sambrana Ivan <sambranaivan@gmail.com>
2018-10-04 18:22:52 -0300

    Maquetado de vistas de admin-ofertas

--- resources/views/oferta.blade.php
+++ resources/views/oferta.blade.php
@@ -3,8 +3,44 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-////lista ofertas publicadas
+        <div class="col-md-10">
+            <div class="card">
+                <div class="card-header">
+                    <div class="row">
+                        <div class="col-md-10"><h2>Ofertas de Posgrado</h2></div>
+                        <div class="col-md-2"><button class="btn btn-success  btn-block">Nueva Oferta</button></div>
+                    </div>
+                </div>
 
+                <div class="card-body">
+                    {{-- ////listar ofertas publicadas --}}
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Nro. Oferta</th>
+                                <th>Carrera</th>
+                                <th>Inicio de Oferta</th>
+                                <th>Expiración Oferta</th>
+                                <th>Inicio de Inscipción</th>
+                                <th>Fin de Inscripción</th>
+                                <th>Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        </tbody>
+
+
+                        </th>
+                    </table>
+
+                </div>
+
+        </div>
+        {{-- //end card --}}
     </div>
+    {{-- //end col --}}
 </div>
+{{-- // end row --}}
+</div>
+{{-- // en container --}}
 @endsection
