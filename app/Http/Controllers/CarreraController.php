858876c79e6d69a1041dc73ee94c1a221216c18c
Sambrana Ivan <sambranaivan@gmail.com>
2018-10-05 18:09:59 -0300

    vistas y controladores para carreras

--- app/Http/Controllers/CarreraController.php
+++ app/Http/Controllers/CarreraController.php
@@ -7,4 +7,13 @@ use Illuminate\Http\Request;
 class CarreraController extends Controller
 {
     //
+
+    public function index(){
+        return view('carreras');
+    }
+
+    public function nuevaCarrera()
+    {
+        return view('nueva_carrera');
+    }
 }
---
+++ resources/views/nueva_carrera.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+<div class="col-md-10">
+            <div class="card">
+                <div class="card-header">
+                    <div class="row">
+                        <div class="col-md-10"><h2>Nueva Carrera</h2></div>
+                    </div>
+                </div>
+            </div>{{-- end card --}}
+
+
+    </div>{{-- end col --}}
+</div>
+{{-- end row --}}
+</div>
+{{-- end container --}}
+@endsection
