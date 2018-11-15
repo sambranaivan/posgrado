6bf72bc96eb207c1c61088f9f18c44541e0ac37d
Sambrana Ivan <sambranaivan@gmail.com>
2018-11-15 17:28:35 -0300

    lista tramites

--- resources/views/tramites.blade.php
+++ resources/views/tramites.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="card">
-    <div class="card-head">
+    <div class="card-header">
         <div class="row">
             <div class="col-md-2"></div>
             <div class="col-md-8">
