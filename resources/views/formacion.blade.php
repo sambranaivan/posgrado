210bef919f9d367361753b4ec65d5c8e14eb0574
Sambrana Ivan <sambranaivan@gmail.com>
2018-11-16 13:28:02 -0300

    formacion continua

--- resources/views/formacion.blade.php
+++ resources/views/formacion.blade.php
@@ -3,13 +3,18 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-<div class="col-md-10">
+<div class="col-md-6">
             <div class="card">
                 <div class="card-header">
-                    <div class="row">
-                        <div class="col-md-10"><h2>Formación Continua</h2></div>
-                    </div>
+                    <h2 class="text-center">Formacion Continua</h2>
                 </div>
+                    <div class="card-body">
+                        <div class="list-group">
+                            <a href="#" class="list-group-item list-group-item-action text-center">Diplomaturas</a>
+
+                            <a href="#" class="list-group-item list-group-item-action text-center">Cursos de Posgrado</a>
+                        </div>
+                    </div>
             </div>{{-- end card --}}
 
 
