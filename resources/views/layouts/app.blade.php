899db37e44fec84cfc70339564c7758b506efac9
Sambrana Ivan <sambranaivan@gmail.com>
2018-12-21 16:32:40 -0300

    estilo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Secretaria General de Posgrado - Universidad Nacional del Nordeste </title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -19,14 +19,21 @@
     <!-- Styles -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+
 </head>
+<style>
+    body{
+        font-family: 'Nunito', sans-serif;
+    }
+</style>
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
+            <div class="container-fluid">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    <img src="{{asset('img/unne.png')}}" width="30" height="30" class="d-inline-block align-top" alt="">
-                    Posgrado
+                    <img src="{{asset('img/logo.png')}}" width="auto" height="50" class="d-inline-block align-top" alt="">
+
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -34,46 +41,43 @@
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="/">
-                               Noticias
-                            </a>
-                        </li>
-                         <li class="nav-item">
-                            <a class="nav-link" href="/carreras">
-                               Carreras
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/tramites">
-                               Guia de Trámites
-                            </a>
-                        </li>
-                         <li class="nav-item">
-                            <a class="nav-link" href="/formacion">
-                               Formación continua
-                            </a>
-                        </li>
-                         <li class="nav-item">
-                            <a class="nav-link" href="/becas">
-                               Becas y Subsidios
-                            </a>
-                        </li>
-                         <li class="nav-item">
-                            <a class="nav-link" href="/eventos">
-                               Eventos
-                            </a>
-                        </li>
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link  align-middle" href="/">
+                               <h6 class="text-center"><b>Noticias</b></h6>
+                            </a>
+                        </li>
+                         <li class="nav-item">
+                            <a class="nav-link  align-middle" href="/carreras">
+                               <h6 class="text-center"><b>Carreras</b></h6>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link  align-middle" href="/tramites">
+                               <h6 class="text-center"><b>Guia de Trámites</b></h6>
+                            </a>
+                        </li>
+                         <li class="nav-item">
+                            <a class="nav-link  align-middle" href="/formacion">
+                               <h6 class="text-center"><b>Formación continua</b></h6>
+                            </a>
+                        </li>
+                         <li class="nav-item">
+                            <a class="nav-link  align-middle" href="/becas">
+                               <h6 class="text-center"><b>Becas y Subsidios</b></h6>
+                            </a>
+                        </li>
+                         <li class="nav-item">
+                            <a class="nav-link  align-middle" href="/eventos">
+                               <h6 class="text-center"><b>Eventos</b></h6>
+                            </a>
+                        </li>
+
+
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Iniciar Sesión') }}</a>
+                                <a class="nav-link" href="{{ route('login') }}"><h6>{{ __('Iniciar Sesión') }}</h6></a>
                             </li>
                             {{-- <li class="nav-item">
                                 @if (Route::has('register'))
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,39 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container-fluid">
     <div class="row justify-content-center">
 <div class="col-md-12">
             <div class="card">
                 <div class="card-header">
                     <div class="row">
-                        <div class="col-md-12"><h2>Noticias</h2></div>
+                        <div class="col-md-12"><h2 class="text-info">Noticias</h2></div>
                     </div>
                 </div>
                 <div class="card-body">
                      <div class="row">
                         <div class="col-md-4">
-                            <h4>La UNNE amplía su presencia a escala continental con nuevas movilidades en 2019</h4>
+
+                            <h4 class="text-info">La UNNE amplía su presencia a escala continental con nuevas movilidades en 2019</h4>
                             <img src="{{asset('img/1.jpg')}}" class="img-thumbnail">
-                            <p>
+                            <p class="text-justify">
                                 A través de la Secretaría General de Relaciones Interinstitucionales, dirigida por el arq. Gustavo Tripaldi, se gestionan proyectos y convenios con universidades de todo el mundo, que año a año ofrecen a estudiantes, docentes y gestores de la UNNE la oportunidad de realizar experiencias de intercambio académico y de gestión institucional, que van desde períodos de dos (2) semanas –docentes y gestores-, hasta un semestre completo para los estudiantes de grado y posgrado.
                             </p>
                         </div>
                          <div class="col-md-4">
-                             <h4>Químico de Dinamarca dictó cursos de Posgrado en Ciencias Exactas</h4>
+                             <h4 class="text-info">Químico de Dinamarca dictó cursos de Posgrado en Ciencias Exactas</h4>
                              <img src="{{asset('img/2.jpg')}}" class="img-thumbnail">
-                             <p>El doctor en Física de la Facultad de Ciencias Exactas de la UNNE, Patricio Provasi, organizó cursos de posgrado sobre Fundamentos del Modelado Molecular e Introducción al Modelado Molecular. Los mismos contaron como profesor dictante al doctor en Química Stephan Sauer de la Universidad de Copenague, Dinamarca.</p>
+                             <p class="text-justify">El doctor en Física de la Facultad de Ciencias Exactas de la UNNE, Patricio Provasi, organizó cursos de posgrado sobre Fundamentos del Modelado Molecular e Introducción al Modelado Molecular. Los mismos contaron como profesor dictante al doctor en Química Stephan Sauer de la Universidad de Copenague, Dinamarca.</p>
                         </div>
                         <div class="col-md-4">
-                             <h4>Se formalizó la doble titulación para la Carrera de Posgrado Maestría en Ciencias de la Ingeniería entre la UNNE y la UFRGS</h4>
+                             <h4 class="text-info">Se formalizó la doble titulación para la Carrera de Posgrado Maestría en Ciencias de la Ingeniería entre la UNNE y la UFRGS</h4>
                              <img src="{{asset('img/3.jpg')}}" class="img-thumbnail">
-                             <p>Autoridades de la Facultad de Ingeniería de la UNNE mantuvieron una reunión con sus pares de la Escola de Engenharia da UFRGS –Brasil-, con el objetivo de avanzar en cuestiones procedimentales del acuerdo de Doble Titulación para la Carrera de Posgrado Maestría en Ciencias de la Ingeniería de esta casa de estudios.</p>
+                             <p class="text-justify">Autoridades de la Facultad de Ingeniería de la UNNE mantuvieron una reunión con sus pares de la Escola de Engenharia da UFRGS –Brasil-, con el objetivo de avanzar en cuestiones procedimentales del acuerdo de Doble Titulación para la Carrera de Posgrado Maestría en Ciencias de la Ingeniería de esta casa de estudios.</p>
                         </div>
-
-
-
-
-
                     </div>
                 </div>
             </div>{{-- end card --}}
