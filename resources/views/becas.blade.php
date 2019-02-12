8e1b81d0f595f3ddeba947911244a91c7286aad2
Sambrana Ivan <sambranaivan@gmail.com>
2019-02-12 08:29:31 -0300

    becas demo

--- resources/views/becas.blade.php
+++ resources/views/becas.blade.php
@@ -7,10 +7,195 @@
         <div class="col-md-12 text-center text-info">
             <h1 class="text-center text-white" style="background-image:url({{asset('img/carreras/fondo.png')}});background-size:cover; ">Becas y Subsidios</h1>
         </div>
-        <div class="col-md-12 text-center">
+        {{--  <div class="col-md-12 text-center">
             <h3 class="text-muted">..En Construcción</h3>
+        </div>  --}}
+    </div>
+    {{--  ///items  --}}
+<div class="row">
+        <div class="col-md-10 offset-md-1">
+<div id="accordion">
+                {{--  //CAROLINA  --}}
+  <div class="card">
+    <div class="card-header" id="headingOne">
+      <h4 class="mb-0">
+        <button class="btn btn-lg btn-block" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+         Fundacion Carolina
+        </button>
+      </h4>
+    </div>
+
+    <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+      <div class="card-body">
+
+
+
+
+<div id="_carolina_accordion">
+
+  <div class="card">
+    <div class="card-header" id="_carolina_headingOne">
+      <h4 class="mb-0">
+        <button class="btn btn-link btn-small" data-toggle="collapse" data-target="#_carolina_collapseOne" aria-expanded="false" aria-controls="_carolina_collapseOne">
+        Becas para cursar másteres y cursos de especialización
+        </button>
+      </h4>
+    </div>
+
+    <div id="_carolina_collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#_carolina_accordion">
+      <div class="card-body">
+        <div class="entry-content-wrapper clearfix">
+<section class="av_textblock_section " itemscope="itemscope" itemtype="https://schema.org/CreativeWork"><div class="avia_textblock  " itemprop="text"><h4>Becas para cursar másteres y cursos de especialización</h4>
+<p class="p1"><span class="s1">Las&nbsp;</span><span class="s2"><b>becas de postgrado</b></span><span class="s1">&nbsp;están dirigidas a la formación en España de licenciados nacionales de un país de América Latina, miembro de la Comunidad Iberoamericana de Naciones, o de Portugal, con capacidad académica o profesional avalada por un currículum sobresaliente.</span></p>
+<p align="justify">El programa ofrece <span style="text-decoration: underline;">dos modalidades de apoyo</span>:</p>
+<ul>
+<li>becas</li>
+<li>ayudas al estudio</li>
+</ul>
+<p>La diferencia principal entre ambas modalidades de ayuda radica en la diferente aportación económica que en cada caso asume la FC, las instituciones académicas españolas y el propio becario.</p>
+<p style="text-align: center;"><a href="http://gestion.fundacioncarolina.es/candidato/becas/programas/programas.asp?tipo_area=0"><strong>SOLICITAR BECA</strong></a></p>
+</div></section>
+<section class="av_textblock_section " itemscope="itemscope" itemtype="https://schema.org/CreativeWork"><div class="avia_textblock  " itemprop="text"><p align="justify"><strong>Advertencia</strong>: La creación del Espacio Europeo de Educación Superior podría implicar algunas modificaciones en ciertos aspectos de los cursos ofertados por la Fundación Carolina, entre otros, el relativo a la duración de los mismos, número de horas, etc.</p>
+<p align="justify">De producirse alguna modificación, esta será anunciada en este apartado y comunicada a título individual a los solicitantes del curso que pudiera verse afectado.</p>
+<p align="justify">Finalmente les informamos que la Fundación no asegura la homologación de las titulaciones españolas en América Latina.</p>
+<p align="justify">
+</p></div></section>
+</div>
+
+      </div>
+    </div>
+  </div>
+
+
+  <div class="card">
+    <div class="card-header" id="_carolina_headingTwo">
+      <h5 class="mb-0">
+        <button class="btn btn-link btn-small" data-toggle="collapse" data-target="#_carolina_collapseTwo" aria-expanded="false" aria-controls="_carolina_collapseTwo">
+        Becas de Doctorado y Estancias cortas postdoctorales
+        </button>
+      </h5>
+    </div>
+    <div id="_carolina_collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#_carolina_accordion">
+      <div class="card-body">
+       <section class="av_textblock_section " itemscope="itemscope" itemtype="https://schema.org/CreativeWork"><div class="avia_textblock  " itemprop="text"><h4>Becas de Doctorado y Estancias cortas postdoctorales</h4>
+<p align="justify">Las <strong>becas de doctorado</strong> están dirigidas a fomentar la obtención del grado académico de doctor entre los docentes de las universidades de América Latina asociadas con la FC, mientras que el objetivo de las becas de Estancias cortas postdoctorales es completar la formación postdoctoral de los profesores de estos centros y facilitar el establecimiento de redes académicas entre España y América.</p>
+<p align="justify">Ambos tipos de becas se basan en un régimen de cofinanciación, fijado en convenios bilaterales de cooperación educativa, inspirado fundamentalmente en el principio de igualdad de oportunidades.</p>
+<p align="justify">El marco general del sistema de cofinanciación contempla que el 60% del coste total de la beca lo cubre la Fundación Carolina y el 40% restante la universidad o institución de América Latina.</p>
+<p align="justify">En un programa como éste, dirigido a fomentar el doctorado exclusivamente dentro del sistema educativo superior en América Latina, el sistema de la cofinanciación resulta muy beneficioso. La ventaja mas apreciable que aporta este programa es que hace copartícipes de estas becas a las universidades latinoamericanas para que estos docentes, una vez alcanzan el grado de doctor, retornen a sus universidades de origen que, en definitiva, son las corresponsables económicas de su formación.</p>
+<p align="justify">Para la consecución de los objetivos señalados, FC negocia la firma de <strong><a title="Convenios" href="http://www.fundacioncarolina.es/formacion/becas-de-doctorado-y-estancias-cortas-postdoctorales/convenios-con-instituciones-academicas/">convenios con universidades o instancias gubernamentales</a></strong> que agrupan a todas las universidades de un mismo país o, por lo menos, a todas las que tienen la consideración de públicas. Esta última fórmula asegura la publicación de convocatorias de carácter nacional en aquellos países donde sea posible la firma de este tipo de acuerdos, con lo que se consigue que este programa resulte accesible para cualquier docente latinoamericano.</p>
+<p align="justify"><strong>Solidaridad universitaria española</strong>: la mayoría de las universidades públicas españolas eximen del pago de las tasas del programa conducente al grado académico de doctor y algunas, incluso, participan en la manutención mensual de los becarios. Esta implicación supone una disminución de la aportación que corresponde a la universidad latinoamericana y a la Fundación Carolina.</p>
+<p><strong>NOTA DE INTERÉS</strong></p>
+<p align="justify">Fundación Universia y Fundación Carolina colaboran conjuntamente con el fin de fomentar y contribuir a hacer efectivo el principio de igualdad para las personas con discapacidad. A tal fin, Fundación Universia, en colaboración con Fundación Carolina pone en marcha un programa de ayudas complementarias para personas con discapacidad acreditada y que hayan sido beneficiarios de una beca de Fundación Carolina. El funcionamiento de estas ayudas se dará a conocer a los beneficiarios de las becas una vez las mismas hayan sido concedidas.</p>
+<p style="text-align: center;" align="justify"><a href="http://gestion.fundacioncarolina.es/candidato/becas/programas/programas.asp?tipo_area=0"><strong>SOLICITAR BECA</strong></a></p>
+</div></section>
+      </div>
+    </div>
+  </div>
+
+</div>
+{{--  end accordion  --}}
+
+
+
+
+
+      </div>
+    </div>
+  </div>
+
+  {{--  //AUIP  --}}
+  <div class="card">
+    <div class="card-header" id="headingTwo">
+      <h5 class="mb-0">
+        <button class="btn btn-lg btn-block" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          AUIP
+        </button>
+      </h5>
+    </div>
+    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+      <div class="card-body">
+
+
+
+{{--  AUIP CONTENTE  --}}
+<div id="_auip_accordion">
+
+  <div class="card">
+    <div class="card-header" id="_auip_headingOne">
+      <h4 class="mb-0">
+        <button class="btn btn-link btn-small" data-toggle="collapse" data-target="#_auip_collapseOne" aria-expanded="false" aria-controls="_auip_collapseOne">
+        Becas para cursar Másteres en Instituto Politécnico de Leiria 2019
+        </button>
+      </h4>
+    </div>
+
+    <div id="_auip_collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#_auip_accordion">
+      <div class="card-body">
+      <div>
+        <p><span class="h2">Becas para cursar Másteres en Instituto Politécnico de Leiria 2019</span></p>
+        <p><strong>Plazo de solicitud:</strong> hasta el 31 de marzo de 2019 a las 23:59 horas (GMT/UTC+01:00) Madrid.</p>
+        <p><strong>Nº de Plazas convocadas:</strong> 12.</p>
+        <p><strong>Cuantía de las Becas:</strong> Las becas financian el 100% de la tasa de matrícula (500 euros) y el 50% del coste anual de matrícula en un master para un alumno extranjero (1.500 euros anuales).</p>
+        <p><strong>Dirigido a:</strong> estudiantes, profesores y egresados de universidades de cualquier país latinoamericano, asociadas a la AUIP, que estén interesados en realizar estudios oficiales de Máster.</p>
+        <p style="text-align: center;"><a href="http://www.auip.org/images/stories/DATOS/PDF/2019/Becas_AUIP/bases_leiria_2019.pdf" target="_blank" class="button subbutton3 btn btn-primary" rel="noopener noreferrer">Bases del programa</a> <a href="http://solicitudes.auip.org" target="_blank" class="button subbutton4 btn btn-primary" rel="noopener noreferrer">Registro de solicitudes</a></p>
+      </div>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="card">
+    <div class="card-header" id="_auip_headingTwo">
+      <h5 class="mb-0">
+        <button class="btn btn-link btn-small" data-toggle="collapse" data-target="#_auip_collapseTwo" aria-expanded="false" aria-controls="_auip_collapseTwo">
+       Programa de Becas de Movilidad Académica entre Instituciones Asociadas a la AUIP 2019
+        </button>
+      </h5>
+    </div>
+    <div id="_auip_collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#_auip_accordion">
+      <div class="card-body">
+       <div>
+        <p><span class="h2">Programa de Becas de Movilidad Académica entre Instituciones Asociadas a la AUIP 2019</span></p>
+        <p><strong>Plazo de solicitud:</strong> <br><strong>Primer plazo:</strong> Para viajes y estancias que se inicien entre <strong>julio y diciembre de 2019</strong>, la convocatoria se cierra el <strong>22 de marzo de 2019 a las 23:59 horas (GMT/UTC+01:00) Madrid.</strong><br><strong>Segundo plazo:</strong> Para viajes y estancias que se inicien entre <strong>enero y junio de 2020</strong>, la convocatoria se cierra el <strong>31 de octubre de 2019 a las 23:59 horas (GMT/UTC+01:00) Madrid</strong>. <strong><em>(NOTA: No se admitirán solicitudes de beca, para este plazo, antes del 23 de marzo de 2019).</em></strong></p>
+        <p><strong>Cuantía de las Becas:</strong> Las ayudas cubren única y exclusivamente el desplazamiento internacional hasta una cuantía máxima de 1.200 euros. Será la AUIP quien, a través de su agente de viajes, facilitará al becario el billete de avión. Para viajes entre España y Portugal, la cuantía de la ayuda será de un máximo de 800 euros y será necesario presentar justificantes o comprobantes de los gastos.</p>
+        <p><strong>Dirigido a:</strong> Profesores e investigadores, Gestores de programas de postgrado y doctorado, Estudiantes de postgrado y doctorado, Interesados en cursar másteres o doctorados.</p>
+        <p style="text-align: center;"><a href="http://www.auip.org/images/stories/DATOS/PDF/2019/Becas_AUIP/bases_movilidad_general2019Ap.pdf" target="_blank" class="button subbutton4 btn btn-primary" rel="noopener noreferrer">Bases del programa</a><a href="http://solicitudes.auip.org" target="_blank" class="button subbutton4 btn btn-primary" rel="noopener noreferrer">Registro de solicitudes</a></p>
+        </div>
+            </div>
+            </div>
+  </div>
+
+
+
+</div>
+{{--  end accordion  --}}
+
+
+
+      </div>
+    </div>
+  </div>
+
+  {{--  /// AUGM  --}}
+  <div class="card">
+    <div class="card-header" id="headingThree">
+      <h5 class="mb-0">
+        <button class="btn btn-lg btn-block" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          AUGM
+        </button>
+      </h5>
+    </div>
+    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+      <div class="card-body">
+
+      </div>
+    </div>
+  </div>
+</div>
+{{--  end accordion  --}}
         </div>
     </div>
+
 {{-- end row --}}
 </div>
 {{-- end container --}}
