98ac75ae774fa7733569197a88d816f7bc4259e9
Sambrana Ivan <sambranaivan@gmail.com>
2019-03-27 16:11:04 -0300

    becas

--- resources/views/becas.blade.php
+++ resources/views/becas.blade.php
@@ -3,57 +3,299 @@
 @section('content')
 <div class="container-fluid">
 
-    <div class="row">
-        <div class="col-md-12 text-center text-info">
-            <h1 class="text-center text-white" style="background-image:url({{asset('img/carreras/fondo.png')}});background-size:cover; ">Becas y Subsidios</h1>
-        </div>
-         <div class="col-md-8 offset-md-2">
+        <div class="row">
+                <div class="col-md-12 text-center text-info">
+                <h1 class="text-center text-white" style="background-image:url({{asset('img/carreras/fondo.png')}});background-size:cover; ">Becas y Subsidios</h1>
+                </div>
+                <div class="col-md-8 offset-md-2">
 
-          <div class="card">
-              <div class="card-body">
-<h3 class="text-center">Becas para realizar Estudios de Máster Universitario en la Universidad Carlos III 2019</h3>
-<ul class="list-group">
+                <div class="card">
+                    <div class="card-header">
+                        .<div class="row">
+                            <div class="col-md-2">
+                                <img  class="card-img-top img-fluid" src="https://ci3.googleusercontent.com/proxy/weJzDNxXIgAxzIKt-elT3n_TMkivvfaKpNyz63IEJlBrjUoutmP_sRHxpM8ugE3tYxWhSDVAs4ijoesrQjJlKZM3pZxq8K5I2-T_JUV8gFqeoV1R=s0-d-e1-ft#http://auip.org/images/stories/DATOS/BANNERS/2019/banner_uc3m.jpg">
+                            </div>
+                            <div class="col-md-10">
+                                <h3 class="text-center">
+                                    Becas para realizar Estudios de Máster Universitario en la Universidad Carlos III 2019
+                                </h3>
+                            </div>
+                        </div>
 
-    {{-- <li class="list-group-item">Item</li> --}}
-    <li><p><strong>Plazo de solicitud:</strong> desde el 6 de marzo hasta el 10 de abril de 2019 (ambos incluidos).</p></li>
+                    </div>
+                        <div class="card-body">
 
-    <li><p><strong>Nº de Plazas convocadas:</strong> 12.</p></li>
+                            <ul class="list-group">
+
+
+                                <li><p><strong>Plazo de solicitud:</strong> desde el 6 de marzo hasta el 10 de abril de 2019 (ambos incluidos).</p></li>
+
+                                <li><p><strong>Nº de Plazas convocadas:</strong> 12.</p></li>
+
+                                <li><strong>Cuantía de las Becas:</strong></li>
+                             </ul>
+
+                                <div class="row">
+                                        <div class="col-md-11  offset-md-1">
+                                            <ul class="list-group">
+                                            <li>
+                                            <p class="text-justify">
+                                                El importe de la matrícula de los créditos ECTS de las asignaturas que el estudiante matricula por primera vez, con un máximo de 60 ECTS. La ayuda no incluye seguros, cargos ni otras tasas asociadas a la matrícula. En el caso de másteres de 90 o 120 créditos ECTS, la ayuda de la matrícula también incluye los créditos que el estudiante matricule por primera vez en el segundo curso, con la limitación de 30 o 60 créditos como máximo, respectivamente.
+                                            </p>
+                                            </li>
+                                            <li>
+                                            <p>
+                                                Una bolsa o ayuda de estudios de 1.500 €.
+                                            </p>
+                                            </li>
+                                            </ul>
+                                        </div>
+
+                                            <p>
+                                            <strong>Dirigido a:</strong> titulados universitarios no españoles egresados de universidades latinoamericanas asociadas a la AUIP.
+                                            </p>
+
+                                            <p>
+                                            <strong>Más información:</strong> <a href="http://auip.org/es/becas-auip/1732">http://auip.org/es/becas-auip/1732</a>
+                                            </p>
+                                </div>
 
 
 
-    <li><strong>Cuantía de las Becas:</strong></li>
-</ul>
-<div class="row">
-    <div class="col-md-10  offset-md-1">
-        <ul class="list-group">
-<li><p>
-    El importe de la matrícula de los créditos ECTS de las asignaturas que el estudiante matricula por primera vez, con un máximo de 60 ECTS. La ayuda no incluye seguros, cargos ni otras tasas asociadas a la matrícula. En el caso de másteres de 90 o 120 créditos ECTS, la ayuda de la matrícula también incluye los créditos que el estudiante matricule por primera vez en el segundo curso, con la limitación de 30 o 60 créditos como máximo, respectivamente.
-</p></li>
-<li><p>
-Una bolsa o ayuda de estudios de 1.500 €.
-</p></li>
-<ul class="list-group">
-    </div>
-    <div>
-<p>
-    <strong>Dirigido a:</strong> titulados universitarios no españoles egresados de universidades latinoamericanas asociadas a la AUIP.
-</p>
+                </div>
 
-<p>
-    <strong>Más información:</strong> <a href="http://auip.org/es/becas-auip/1732">http://auip.org/es/becas-auip/1732</a>
-    </p>
-
-</p>
-</div> </div>
-                  </li>
-
-              </ul>
+        </div>{{-- end row --}}
+        {{-- OTRA BECA --}}
+        <div class="card text-left">
+          <div class="card-header">
+              <div class="row">
+                  <div class="col-md-2">
+                      <img class="card-img-left img-fluid" src="https://ci3.googleusercontent.com/proxy/lLBia3j_0VTdzF2dX-71y8MNB4RJnbs40vfK_EGoT2P_oQVcRSZ1n0XuM9EOUJUNV1g-cxYJybmYRUG50uhqjuAzggufIvarzZPwGySxTXogqH4hoXuCsl3OfqrK=s0-d-e1-ft#http://auip.org/images/stories/DATOS/BANNERS/2019/banner_pablo_olavide.jpg" alt="">
+                  </div>
+                  <div class="col-md-10">
+                                <h3 class="text-center">
+                                 Becas para cursar Másteres Universitarios en la Universidad Pablo de Olavide de Sevilla 2019
+                                </h3>
+                            </div>
+              </div>
           </div>
+          <div class="card-body">
 
 
+                 <ul class="list-group">
+                     <li>
+                <p>
+                        <strong>Plazo de solicitud</strong>: hasta el 9 de mayo de 2019 a las 23:59 horas (GMT/UTC+01:00) Madrid.
+                    </p>
+                    </li>
+
+                    <li>
+                <p>
+                        <strong>Nº de Plazas convocadas</strong> 15.
+                    </p>
+                    </li>
+
+                    <li>
+                <p>
+                        <strong>Cuantía de las Becas:</strong>
+                    </p>
+                    </li>
+                                </ul>
+                <div class="row">
+                    <div class="col-md-11 offset-md-1">
+                        <ul class="list-group">
+
+                        <li><p class="text-justify">
+                            Las becas financiarán el alojamiento en la Residencia Universitaria Flora Tristán. Incluyen además un seguro de asistencia médico-hospitalaria y de accidentes así como una ayuda para gastos de manutención por un total de 180 € mensuales.
+                        Los estudiantes que obtengan beca podrán tener derecho también a una reducción del costo de la matrícula hasta igualar el precio para ciudadanos residentes en España si así lo contempla el Decreto de Precios Públicos u Orden Adicional de la Consejería de Economía, Innovación, Ciencia y Empleo de la Junta de Andalucía.
+                        </p></li>
+                </ul>
+                </div>
+
+                    </div>
+                    <p>
+                <strong>Dirigido a:</strong> estudiantes, profesores y egresados, con residencia legal en países distintos a España, de universidades extranjeras, preferentemente asociadas a la AUIP, interesados en realizar un Máster Universitario.</li>
+                    </p>
+                    <p>
+                <strong>Más información:</strong> <a href="http://auip.org/es/becas-auip/1758">http://auip.org/es/becas-auip/1758</a>
+                    </p>
+          </div>
         </div>
-    </div>
-    {{--  ///items  --}}
+        {{--  --}}
+
+ <div class="card text-left">
+          <div class="card-header">
+              <div class="row">
+                  <div class="col-md-2">
+                      <img class="card-img-left img-fluid" src="https://ci5.googleusercontent.com/proxy/BTliJkDbnD5NwZrBOG9JDXAfpbDi97XiL2AxMzFj0NFp4xV4vjOXCHJ75XBL9wR8nsNFZH2yDQblxkjG03a9mdxyHBTewCJOZq2ogax7rCM7wI1Z64iC=s0-d-e1-ft#http://auip.org/images/stories/DATOS/BANNERS/2019/banner_sevilla.jpg" alt="">
+                  </div>
+                  <div class="col-md-10">
+                                <h3 class="text-center">
+                               Becas para cursar Másteres Universitarios en la Universidad de Sevilla 2019
+                                </h3>
+                            </div>
+              </div>
+          </div>
+          <div class="card-body">
+
+
+                 <ul class="list-group">
+                     <li>
+                <p>
+                        <strong>Plazo de solicitud</strong>: hasta el 9 de mayo de 2019 a las 23:59 horas (GMT/UTC+01:00) Madrid.
+                    </p>
+                    </li>
+
+                    <li>
+                <p>
+                    <strong>Nº de Plazas convocadas</strong> 10.
+                </p>
+                </li>
+
+                <li>
+                <p>
+                        <strong>Cuantía de las Becas:</strong>
+                    </p>
+                    </li>
+                                </ul>
+                <div class="row">
+                    <div class="col-md-11 offset-md-1">
+                        <ul class="list-group">
+
+                        <li><p class="text-justify">
+                           2.500€. Las becas cubren los gastos de matrícula de un máster de 60 ECTS. La cantidad restante hasta los 2.500 euros se hará efectiva, una vez el becario haya formalizado su matrícula e iniciado los estudios correspondientes en la Universidad de Sevilla.
+                        </p></li>
+                </ul>
+                </div>
+
+                    </div>
+
+                    <p>
+                <strong>Dirigido a:</strong> estudiantes, profesores y egresados vinculados a instituciones del ámbito latinoamericano asociadas a la AUIP que estén interesados en realizar estudios oficiales de Máster Universitario.
+            </p>
+
+                    <p>
+                <strong>Más información:</strong> <a href="http://auip.org/es/becas-auip/1756">http://auip.org/es/becas-auip/1756</a>
+                    </p>
+          </div>
+        </div>
+
+
+        {{--  --}}
+        <div class="card text-left">
+          <div class="card-header">
+              <div class="row">
+                  <div class="col-md-2">
+                      <img class="card-img-left img-fluid" src="https://ci6.googleusercontent.com/proxy/IMg9fD8U48acIkgIZjoTzJL-qlPR_OeUO7v5F7ut43-7oQjUp8ikXXxUWG34HVkNGdq2KhTGmUXJpGN4K6wfIsvFD0Hna5YJa4CjP6bhMtGcFYRf=s0-d-e1-ft#http://auip.org/images/stories/DATOS/BANNERS/2019/banner_jaen.jpg" alt="">
+                  </div>
+                  <div class="col-md-10">
+                                <h3 class="text-center">
+                                    Becas para cursar Másteres Universitarios en la Universidad de Jaén 2019
+                                </h3>
+                            </div>
+              </div>
+          </div>
+          <div class="card-body">
+
+
+                 <ul class="list-group">
+                     <li>
+                <p>
+                        <strong>Plazo de solicitud</strong>:hasta el 9 de mayo de 2019 a las 23:59 horas (GMT/UTC+01:00) Madrid.
+                    </p>
+                    </li>
+
+                    <li>
+                <p>
+                    <strong>Nº de Plazas convocadas</strong> 18.
+                </p>
+                </li>
+
+                <li>
+                <p>
+                        <strong>Cuantía de las Becas:</strong>
+                    </p>
+                    </li>
+                                </ul>
+                <div class="row">
+                    <div class="col-md-11 offset-md-1">
+                        <ul class="list-group">
+
+                        <li><p class="text-justify">
+                           Las becas cubren los costos de matrícula de 60 créditos al programa elegido, incluidas las tasas administrativas. Las becas además tienen una dotación de 4.000 euros en efectivo para contribuir a los gastos de desplazamiento internacional, estancia, alojamiento y manutención.
+                        </p></li>
+                </ul>
+                </div>
+
+                    </div>
+
+                    <p>
+                <strong>Dirigido a:</strong>  estudiantes, profesores y egresados vinculados a instituciones del ámbito latinoamericano asociadas a la AUIP que estén interesados en realizar estudios oficiales de Máster Universitario.
+            </p>
+
+                    <p>
+                <strong>Más información:</strong> <a href="http://auip.org/es/becas-auip/1752">http://auip.org/es/becas-auip/1752</a>
+                    </p>
+          </div>
+        </div>
+        {{--  --}}
+                <div class="card text-left">
+                        <div class="card-header">
+                            <div class="row">
+                                <div class="col-md-2">
+                                    <img class="card-img-left img-fluid" src="https://ci6.googleusercontent.com/proxy/HA2n4nEqLCQltvLfyIcshd7lDFrd8G15pU5n5vCwsklQheFAsTbli7lR2oNfgXCtstfK-QIy-Avs-Xd_wyPcmUNoxOno5015u5Wac7rtCIiN61Zh-3w=s0-d-e1-ft#http://auip.org/images/stories/DATOS/BANNERS/2019/banner_huelva.jpg" alt="">
+                                </div>
+                                <div class="col-md-10">
+                                                <h3 class="text-center">
+                                                    Becas para cursar Másteres Universitarios en la Universidad de Huelva 2019
+                                                </h3>
+                                            </div>
+                            </div>
+                        </div>
+                        <div class="card-body">
+
+
+                                <ul class="list-group">
+                                    <li>
+                                <p>
+                                        <strong>Plazo de solicitud</strong>:hasta el 9 de mayo de 2019 a las 23:59 horas (GMT/UTC+01:00) Madrid.
+                                    </p>
+                                    </li>
+
+                                    <li>
+                                <p>
+                                    <strong>Nº de Plazas convocadas</strong> 10.
+                                </p>
+                                </li>
+
+                                <li>
+                                <p>
+                                        <strong>Cuantía de las Becas:</strong>
+                                    </p>
+                                    </li>
+                                                </ul>
+                                <div class="row">
+                                    <div class="col-md-11 offset-md-1">
+                                        <ul class="list-group">
+
+                                        <li><p class="text-justify">
+                                        La matrícula correspondiente a 60 créditos ECTS. El pago de las tasas de secretaría correrá por cuenta del/la beneficiario/a. Todas las becas incluyen, además, una bolsa de viaje cuya dotación es de 1.500 euros.
+                                        </p></li>
+                                </ul>
+                                </div>
+
+                                    </div>
+
+                                    <p>
+                                <strong>Dirigido a:</strong>  egresados/as, profesores/as o estudiantes de universidades de América Latina, preferentemente asociadas a la AUIP, que estén interesadas en cursar un Máster Oficial en la Universidad de Huelva.
+                            </p>
+
+                                    <p>
+                                <strong>Más información:</strong> <a href="http://auip.org/es/becas-auip/1752">http://auip.org/es/becas-auip/1752</a>
+                                    </p>
+                        </div>
+                        </div>
+        {{--  --}}
 
 </div>
 {{-- end container --}}
