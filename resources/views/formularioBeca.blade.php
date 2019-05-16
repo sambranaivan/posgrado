ae76c3ff56209b3e23202197de8e157d7554188c
Sambrana Ivan <sambranaivan@gmail.com>
2019-05-16 18:48:48 -0300

    test pull

---
+++ resources/views/formularioBeca.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+<div class="col-md-10">
+            <div class="card">
+                <div class="card-header">
+                    <div class="row">
+                        <div class="col-md-10"><h2>Ofertas de Posgrado</h2></div>
+                    </div>
+                </div>
+                <div class="card-body">
+
+                </div>
+            </div>{{-- end card --}}
+
+
+    </div>{{-- end col --}}
+</div>
+{{-- end row --}}
+</div>
+{{-- end container --}}
+@endsection
