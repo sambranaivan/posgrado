50b9da96572aeddcdd1eff7d7daca039a7fbc0e7
Sambrana Ivan <sambranaivan@gmail.com>
2019-05-17 13:50:09 -0300

    beacs macro

--- resources/views/becas.blade.php
+++ resources/views/becas.blade.php
@@ -29,11 +29,11 @@ De La Red De Macrouniversidades Públicas De América Latina Y El Caribe
                                                 </div>
                                                 <div class="col-md-4">
                                                     {{-- <div class="btn-group-vertical"> --}}
-                                                        <a target="_blank" href="{{asset('pdf/procedimiento_de_postulacion_para_estudiantes.pdf')}}" class="btn btn-block btn-sm btn-primary">
+                                                        <a target="_blank" href="{{asset('pdf/Carta_compromiso_para_la_adquisición_de_seguro_de_gastos_médicos.docx')}}" class="btn btn-block btn-sm btn-primary">
                                                            Carta de Compromiso
                                                            <i class="fas fa-file-word"></i>
                                                            </a>
-                                                           <a target="_blank" href="{{asset('pdf/procedimiento_de_postulacion_para_estudiantes.pdf')}}" class="btn btn-block btn-sm btn-primary">
+                                                           <a target="_blank" href="{{asset('pdf/FORMATO_DE_SOLICITUD_Y_PLAN_DE_TRABAJO.docx')}}" class="btn btn-block btn-sm btn-primary">
                                                            Formato de solicitud y Plan de Trabajo
                                                            <i class="fas fa-file-word"></i>
                                                            </a>
@@ -45,9 +45,9 @@ De La Red De Macrouniversidades Públicas De América Latina Y El Caribe
 
                                                            <a target="_blank" href="{{asset('pdf/procedimiento_de_postulacion_para_estudiantes.pdf')}}" class="btn btn-block btn-sm btn-primary">
                                                            Procedimiento de postulación
-                                                           <i class="fas fa-file-word"></i>
+                                                           <i class="fas fa-file-pdf"></i>
                                                            </a>
-                                                            <a target="_blank" href="{{asset('pdf/procedimiento_de_postulacion_para_estudiantes.pdf')}}" class="btn btn-block btn-sm btn-primary">
+                                                            <a target="_blank" href="{{asset('pdf/Programa-2019-X Convocatoria_PMP.docx')}}" class="btn btn-block btn-sm btn-primary">
                                                            Programa X Convocatoria 2019
                                                            <i class="fas fa-file-word"></i>
                                                            </a>
@@ -57,9 +57,9 @@ De La Red De Macrouniversidades Públicas De América Latina Y El Caribe
                                                 <div class="col-md-4">
                                                     {{-- <div class="btn-group-vertical"> --}}
 
-                                                            <a target="_blank" href="{{asset('pdf/procedimiento_de_postulacion_para_estudiantes.pdf')}}" class="btn btn-block btn-sm btn-danger">
+                                                            <a target="_blank" href="{{asset('pdf/PROTOCOLO_PARA_POSTULACIONES_EN_LA_UNNE.docx')}}" class="btn btn-block btn-sm btn-danger">
                                                            Protocolo para postularse en la UNNE
-                                                           <i class="fas fa-file-pdf"></i>
+                                                           <i class="fas fa-file-word"></i>
                                                            </a>
                                                     {{-- </div> --}}
                                                 </div>
