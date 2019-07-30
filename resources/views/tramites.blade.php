0f09dc134b3fb0a46599bdd37f883c1eeaae22d0
Sambrana Ivan <sambranaivan@gmail.com>
2019-07-30 16:35:52 -0300

    Tramites de adicionales

--- resources/views/tramites.blade.php
+++ resources/views/tramites.blade.php
@@ -10,43 +10,27 @@
         <div class="col-md-12 text-center text-info">
             <h1 class="text-center text-white" style="background-image:url({{asset('img/carreras/fondo.png')}});background-size:cover; ">Guía de Trámites</h1>
         </div>
-        <div class="col-md-12 text-center">
-            <h3 class="text-muted">..En Construcción</h3>
+        <div class="col-md-8 offset-md-2 text-center">
+            <ul class="list-group">
+            <li class="list-group-item text-left">
+                <div class="row">
+
+                    <div class="col-md-8"><h5>Solicitud de Adicional por Título de Especialista o Magister</h5></div><div class="col-md-4 text-right">
+                    <a class="btn btn-danger" href="{{asset('pdf/tramites/adicional_especilista_magister.pdf')}}">Descargar Normativa&nbsp;&nbsp;<i class="fa fa-file-pdf" aria-hidden="true"></i></a></div>
+                </div>
+            </li>
+            <li class="list-group-item text-left">
+                <div class="row">
+
+                    <div class="col-md-8"><h5>Solicitud de Adicional por Título de Doctor</h5></div><div class="col-md-4 text-right">
+                    <a class="btn btn-danger" href="{{asset('pdf/tramites/adicional_doctor.pdf')}}">Descargar Normativa&nbsp;&nbsp;<i class="fa fa-file-pdf" aria-hidden="true"></i></a></div>
+                </div>
+            </li>
+        </ul>
         </div>
     </div>
-{{-- end row --}}
 </div>
-{{-- end container --}}
+
 @endsection
 
-{{-- <div class="container">
-    <div class="card">
-    <div class="card-header">
-        <div class="row">
-            <div class="col-md-2"></div>
-            <div class="col-md-8">
-                 <h4 class="text-center text-info text-center"> Guia de Trámites</h4>
-            </div>
-            <div class="col-md-2">
-                @auth
-                    @if(Auth::user()->id == 1)
-
-                        <a href="nuevotramite" class="btn btn-primary" href="#" role="button">Nuevo Trámite</a>
-                    @endif
-                @endauth
-            </div>
-        </div>
-    </div>
-      <div class="card-body">
-        <div class="list-group">
-        @foreach ($tramites as $item)
-        <a href="{{asset('pdf/tramites/'.$item->file)}}" class="list-group-item list-group-item-action"><b>{{$item->titulo}}</b></a>
-        @endforeach
-
-    </div>
-      </div>
-    </div>
-
-</div> --}}
-{{-- end container --}}
 @endsection
