484c5d1ef7bfce1d5a105907a111d12187f25b98
Ivan Sambrana <sambranaivan@gmail.com>
2020-05-13 11:01:17 -0300

    subo pdf de emovies

--- resources/views/emovies.blade.php
+++ resources/views/emovies.blade.php
@@ -8,7 +8,8 @@
             <h1 class="text-center text-white" style="background-image:url({{asset('img/carreras/fondo.png')}});background-size:cover; ">eMOVIES</h1>
         </div>
         <div class="col-md-12 text-center">
-            <h3 class="text-muted">..En Construcción</h3>
+        <embed src="{{asset('protocolo_emovies_esp_2020.pdf')}}" class="w-75" height="700px" alt="pdf" >
+
         </div>
     </div>
 {{-- end row --}}
