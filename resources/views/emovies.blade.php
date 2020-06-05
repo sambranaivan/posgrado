bbfd2eff75ca4a4dfb475a3e595c8b6d9fc88f57
ivan alejandro sambrana <=>
2020-06-05 11:46:21 -0300

    quito tabla y pongo boton

--- resources/views/emovies.blade.php
+++ resources/views/emovies.blade.php
@@ -5,15 +5,20 @@
 
     <div class="row">
         <div class="col-md-12 text-center text-info">
-            <h1 class="text-center text-white" style="background-image:url({{asset('img/carreras/fondo.png')}});background-size:cover; ">eMOVIES</h1>
-            <h1>ESPACIO DE MOVILIDAD VIRTUAL EN EDUCACIÓN SUPERIOR - eMOVIES (Periodo 2020 - II)<h1>
+            <h1 class="text-center text-white" style="background-image:url({{asset('img/carreras/fondo.png')}});background-size:cover; ">eMOVIES</h2>
+            <h1>ESPACIO DE MOVILIDAD VIRTUAL EN EDUCACIÓN SUPERIOR </br>eMOVIES (Periodo 2020 - II)<h2>
         </div>
         <div class="col-md-12 text-center">
         <embed src="{{asset('protocolo_emovies_esp_2020.pdf')}}" class="w-75" height="700px" alt="pdf" >
 
         </div>
     </div>
-    <div class="row">
+    <div class="row pt-3">
+        <div class="col-12 text-center">
+            <a href="https://docs.google.com/spreadsheets/d/1607jNs0w7-ASRkSC_k53YVpmUa_ozu3ccg27bgEsnMQ/edit?usp=sharing" class="btn btn-lg btn-success">IR AL CATÁLOGO DE CURSOS 2020-II</a>
+        </div>
+    </div>
+    <div class="row d-none">
         <div class="card">
             <div class="card-header text-center">
                
@@ -21,7 +26,7 @@
                     
             </div>
             <div class="card-body">
-                @include('emovies_catalogo')
+                {{-- @include('emovies_catalogo') --}}
             </div>
         </div>
     </div>
