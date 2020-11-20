5d065673c270cd4e6d7e65fdc1ee53ca733114fe
ivan alejandro sambrana <=>
2020-11-20 17:55:52 -0300

    subo resutlados 2020

--- resources/views/subsidios.blade.php
+++ resources/views/subsidios.blade.php
@@ -61,14 +61,16 @@
 </h4></b>
         
         <ol>
-        <li>	<b>Docentes y Becarios</b>: <a target="_blank" href="{{asset('doc/2020-208-Convocatoria 2020 Subsidios Docentes.pdf')}}"> Res. 208/20 CS</a></li>
-        {{-- <li>	Acceso para presentar la información requerida en la postulación: <a target="_blank" href="{{route('inscripcion')}}">Formulario de Inscripción</a></li> --}}
-        <li>Resultados de la convocatoria: <a target="_blank" href="{{asset('doc/Res-295.20-Beneficiarios de subsidios 2020 - Docentes y Becarios.pdf')}}"> Res. 295/20 CS</a></li>
-        {{-- <li>    Modelo de nota solicitud Subsidios Docentes y Becarios: <a href="{{asset('doc/Solicitud de postulación a la convocatoria 2020 de subsidios para Docentes y becarios.docx')}}">Aqui</a></li> --}}
-        <li>	<b>No Docentes</b>: <a target="_blank" href="{{asset('doc/2020-207-Convocatoria 2020 Subsidios No Docente.pdf')}}"> Res. 207/20 CS</a></li>
-        {{-- <li>	Acceso para presentar la información requerida en la postulación: <a target="_blank" href="{{route('inscripcionNoDocente')}}">Formulario de Inscripción</a></li> --}}
-        {{-- <li>Modelo de nota solicitud Subsidios No Docentes: <a href="{{asset('doc/Solicitud de postulación a la convocatoria 2020 de subsidios para No Docentes.docx')}}">Aqui</a></li> --}}
-        <li>Resultados de la convocatoria: <a target="_blank" href="{{asset('doc/Res-320.20 - Beneficiarios de subsidios 2020 - No Docentes.pdf')}}"> Res. 320/20 CS</a></li>
+                <li>	<b>Docentes y Becarios</b>: <a target="_blank" href="{{asset('doc/2020-208-Convocatoria 2020 Subsidios Docentes.pdf')}}"> Res. 208/20 CS</a></li>
+                {{-- <li>	Acceso para presentar la información requerida en la postulación: <a target="_blank" href="{{route('inscripcion')}}">Formulario de Inscripción</a></li> --}}
+                <li>Resultados de la convocatoria: <a target="_blank" href="{{asset('doc/Res-295.20-Beneficiarios de subsidios 2020 - Docentes y Becarios.pdf')}}"> Res. 295/20 CS</a></li>
+                 <li>Cuota 2020 de convocatorias anteriores Docentes y Becarios <a target="_blank" href="{{asset('doc/2020-236-Subsidios Docentes y Becarios Cuota 2020.pdf')}}">Conv. 2019-2018-2017</a></li>
+                {{-- <li>    Modelo de nota solicitud Subsidios Docentes y Becarios: <a href="{{asset('doc/Solicitud de postulación a la convocatoria 2020 de subsidios para Docentes y becarios.docx')}}">Aqui</a></li> --}}
+                <li>	<b>No Docentes</b>: <a target="_blank" href="{{asset('doc/2020-207-Convocatoria 2020 Subsidios No Docente.pdf')}}"> Res. 207/20 CS</a></li>
+                {{-- <li>	Acceso para presentar la información requerida en la postulación: <a target="_blank" href="{{route('inscripcionNoDocente')}}">Formulario de Inscripción</a></li> --}}
+                {{-- <li>Modelo de nota solicitud Subsidios No Docentes: <a href="{{asset('doc/Solicitud de postulación a la convocatoria 2020 de subsidios para No Docentes.docx')}}">Aqui</a></li> --}}
+                <li>Resultados de la convocatoria: <a target="_blank" href="{{asset('doc/Res-320.20 - Beneficiarios de subsidios 2020 - No Docentes.pdf')}}"> Res. 320/20 CS</a></li>
+                 <li>Cuota 2020 de convocatorias anteriores <a target="_blank" href="{{asset('doc/2020-237-Subsidios No docentes cuota 2020.pdf')}}">Conv.2019-2018</a></li>
         </ol>
         <hr>
 <h5>Convocatoria 2019 </h5>
