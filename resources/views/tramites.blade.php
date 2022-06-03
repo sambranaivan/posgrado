6be6fcc16d80c91edd4b57ae894b9d12fbfc48ba
ivan alejandro sambrana <=>
2022-06-03 03:07:19 -0300

    fix

--- resources/views/tramites.blade.php
+++ resources/views/tramites.blade.php
@@ -1,6 +1,4 @@
-@extends('layouts.app')
 
-@section('content')
 @extends('layouts.app')
 
 @section('content')
@@ -33,26 +31,10 @@
                     </p>
                 </div>
             </div>
-            {{-- <ul class="list-group">
-            <li class="list-group-item text-left">
-                <div class="row">
-
-                    <div class="col-md-8"><h5>Solicitud de Adicional por Título de Especialista o Magister</h5></div><div class="col-md-4 text-right">
-                    <a class="btn btn-danger" href="{{asset('pdf/tramites/adicional_especilista_magister.pdf')}}">Descargar Normativa&nbsp;&nbsp;<i class="fa fa-file-pdf" aria-hidden="true"></i></a></div>
-                </div>
-            </li>
-            <li class="list-group-item text-left">
-                <div class="row">
-
-                    <div class="col-md-8"><h5>Solicitud de Adicional por Título de Doctor</h5></div><div class="col-md-4 text-right">
-                    <a class="btn btn-danger" href="{{asset('pdf/tramites/adicional_doctor.pdf')}}">Descargar Normativa&nbsp;&nbsp;<i class="fa fa-file-pdf" aria-hidden="true"></i></a></div>
-                </div>
-            </li>
-        </ul> --}}
+           
         </div>
     </div>
 </div>
 
 @endsection
 
-@endsection
